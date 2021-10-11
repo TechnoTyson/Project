d7f510ef6d163a0c3c4163bcebfb81f8a678598b
TechnoTyson <tysonthegreat72@gmail.com>
2021-10-11 10:44:21 +1030

    OOP:Moved files to lib folder

---
+++ project/lib/Player.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Property.h"
+#include "Tile.h"
+
+class Player
+{
+    //Private variables
+    int position;
+    std::string name;
+    int balance;
+    std::vector<Property> properties;
+    bool gaol_status;
+
+    public:
+    //Public function
+    Player(); //Default Constructor
+    Player(std::string); //Constructor
+
+    //Getters
+    std::vector<Property> getProperties();
+    int getPosition();
+    std::string getName();
+    int getBalance();
+    bool getGaolStatus();
+
+    //Setters
+    void setGaolStatus(bool);
+    void setBalance(int);
+    void setPosition(int);
+
+    //Add a property to player
+    void addProperty(Property);
+};
---
+++ project/lib/Property.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Tile.h"
+
+class Property : public Tile
+{
+    //Private variables
+    int price;
+    std::string colour;
+    int rent;
+    bool ownership_status;
+
+    public:
+    //Public Functions
+    Property(int, std::string, int);
+
+    //Setters
+    void setPrice(int);
+    void setRent(int);
+    void setOwnershipStatus(bool);
+    //Getters
+    int getPrice();
+    int getRent();
+    bool getOwnershipStatus();
+
+    //Run funtion
+    // void run();
+};
