eef9f46687265ea65f3f72a3ff459b57a74255af
TechnoTyson <tysonthegreat72@gmail.com>
2021-10-21 14:45:49 +1030

    OOP:fixed go

--- project/lib/Go.h
+++ project/lib/Go.h
@@ -14,8 +14,4 @@ class Go: public Tile
         void pass_go(Player* player);
         void run(Player* player) override;
         Go();
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 3d3ade2850de65b7018dffd3086ced276660d833
